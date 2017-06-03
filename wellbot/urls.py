8db33554ea502059b2bafd71e070cfc1455d40b2
Sviatoslav Pikh <svyatoslav.pikh@gmail.com>
2017-06-03 23:30:14 +0300

    .

--- wellbot/urls.py
+++ wellbot/urls.py
@@ -8,7 +8,6 @@ from user_menu import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^db', views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user_menu/', views.index, name="user_menu"),
 ]
